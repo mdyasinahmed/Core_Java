8fa1cc3b4d39f873d389d3dc7cc61bb9ef7ccf51
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-01 23:18:19 +0600

    java variable code added

---
+++ Java/Introduction-to-Java/Java-Introduction/HelloJava/Java_Variables.java
@@ -0,0 +1,17 @@
+package HelloJava;
+
+public class Java_Variables {
+	public static void main(String[] args) {
+		int speedLimit = 80;
+		float b = (float) 2.5;
+		double marks = 95.50;
+		char c = 'A';
+		
+		
+		
+		System.out.println(speedLimit);
+		System.out.println(marks);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
