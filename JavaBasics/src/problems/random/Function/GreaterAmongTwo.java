f9f02c20cda670fab952b84e949b97944a97f6fe
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-06 03:55:16 +0600

    Greater Among 2 number problem solved with java function

--- JavaBasics/src/problems/random/Function/GreaterAmongTwo.java
+++ JavaBasics/src/problems/random/Function/GreaterAmongTwo.java
@@ -8,5 +8,15 @@ public class GreaterAmongTwo{
             return b;
         }
     }
-    public static void main(String args)
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int x, y, result;
+
+        System.out.print("Enter Two Numbers: ");
+        x = scan.nextInt();
+        y = scan.nextInt();
+        result = Greater(x,y);
+
+        System.out.println("Greater Among Two is " + result);
+    }
 }
