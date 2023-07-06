cf1c488d2c285c981ad8eeb15fdd94582aa2a4eb
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-06 23:37:56 +0600

    Array Printing using loop code stored

--- JavaBasics/src/java-array/ArraysIntro.java
+++ JavaBasics/src/java-array/ArraysIntro.java
@@ -1,6 +1,7 @@
-public class Arrays{
+public class ArraysIntro{
     public static void main(String args[]){
         // Declaring an Array
+        //int[] marks = new int [3];
         int marks[] = new int [3];
 
         // Storing Array element
@@ -9,8 +10,15 @@ public class Arrays{
         marks[2] = 95;
         
         // Printing Array Manually
+        /*
         System.out.println(marks[0]);
         System.out.println(marks[1]);
         System.out.println(marks[2]);
+        */
+
+        // printing array dynamically
+        for(int i = 0; i<3; i++){
+            System.out.print(marks[i]+" ");
+        }
     }
 }
