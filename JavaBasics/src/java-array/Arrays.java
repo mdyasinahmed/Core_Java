940b14a1624208ebae5bb37a9634db14fde75b7c
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-06 23:34:04 +0600

    java array code added

---
+++ JavaBasics/src/java-array/Arrays.java
@@ -0,0 +1,16 @@
+public class Arrays{
+    public static void main(String args[]){
+        // Declaring an Array
+        int marks[] = new int [3];
+
+        // Storing Array element
+        marks[0] = 97;
+        marks[1] = 98;
+        marks[2] = 95;
+        
+        // Printing Array Manually
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
