2f86c3837ae77c155bb5555ba00ac3c36131b717
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-06 23:55:52 +0600

    Java 2D arrays code added

---
+++ JavaBasics/src/java-array/TwoDArray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class TwoDArray{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter Rows, Cols Size: ");
+        int rows = scan.nextInt();
+        int cols = scan.nextInt();
+
+        int arr[][] = new int[rows][cols];
+
+        // scanning array
+        for(int i=0; i<rows; i++){
+            for(){
+
+            }
+        }
+
+        // printing array
+        for(int i=0; i<size; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
