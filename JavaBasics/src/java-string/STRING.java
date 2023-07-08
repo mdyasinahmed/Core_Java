677699989f95b0264873cd95ed6fd226135a929d
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-08 23:42:20 +0600

    java string scan code added

--- JavaBasics/src/java-string/STRING.java
+++ JavaBasics/src/java-string/STRING.java
@@ -1,10 +1,19 @@
+import java.util.*;
+
 public class STRING{
-    public static main void(String args[]){
+    public static void main(String args[]){
         // Declaration
         String name = "Metro";
-        String fullName = "Metro Plotan"
-        String Sentence = "Metropolitan University"
-        
+        String fullName = "Metro Plotan";
+        String Sentence = "Metropolitan University";
 
+        // dynamic string
+        Scanner scan = new Scanner(System.in);
+
+        String nickName = scan.next();
+        System.out.println("Your Nickname is: " + nickName);
+
+        String fullN = scan.nextLine();
+        System.out.println("Your Full Name is: " + fullN);
     }
 }
