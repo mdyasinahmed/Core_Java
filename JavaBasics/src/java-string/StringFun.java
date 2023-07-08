fa23d900541bbd3b9c4e1fcb6dfc29757cdfc522
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-08 23:51:40 +0600

    java charAt string function added

--- JavaBasics/src/java-string/StringFun.java
+++ JavaBasics/src/java-string/StringFun.java
@@ -7,7 +7,12 @@ public class StringFun{
         String full = s1 + s2;
         System.out.println(full);
 
-        //length
+        // length
         System.out.println("String Length is: " + full.length());
+
+        // charAT
+        // for(int i = 0; i<full.length; i++){
+        //     System.out.println(full.charAt[i]);
+        // }
     }
 }
