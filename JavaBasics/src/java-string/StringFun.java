b0f07f5e100503886f93183b6ff49a140f95f16c
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-08 23:46:57 +0600

    java string concatenation code added

---
+++ JavaBasics/src/java-string/StringFun.java
@@ -0,0 +1,10 @@
+public class StringFun{
+    public static void main(String args[]){
+        // concatenation
+        String s1 = "metro";
+        String s2 = "politan";
+
+        String full = s1 + s2;
+        System.out.println(full);
+    }
+}
