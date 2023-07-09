3468f7747580af60967d001c38e507ce875797a9
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-09 23:25:45 +0600

    java string builder code added

--- JavaBasics/src/java-stringBuilder/stringBuilder.java
+++ JavaBasics/src/java-stringBuilder/stringBuilder.java
@@ -1,5 +1,11 @@
 public class stringBuilder{
     public static void main(String args[]){
-        
+        String str = "h";
+        str += "e";
+        str += "l";
+        str += "l";
+        str += "o";
+
+        System.out.println(str);
     }
 }
