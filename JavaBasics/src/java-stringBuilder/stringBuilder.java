bbe099c2769a4d888ebcd5f1c958bfd8ea2f783e
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-09 23:35:32 +0600

    StringBuilder function code added

--- JavaBasics/src/java-stringBuilder/stringBuilder.java
+++ JavaBasics/src/java-stringBuilder/stringBuilder.java
@@ -7,5 +7,33 @@ public class stringBuilder{
         str += "o";
 
         System.out.println(str);
+
+
+        //Declaring String builder
+        StringBuilder sb = new StringBuilder("Yasin");
+        System.out.println(sb);
+
+        // String Builder Functions:
+        /**
+         * CharAt
+         * Set Character At Index
+         * Insert
+         * Delete
+         * Append
+         * Length
+         */
+        // CharAt
+        System.out.println(sb.charAt(0)); // -> Y
+
+        // Set Char at Index
+        // sb = sb.SetChar(0,E);
+
+        //insert
+        sb.insert(0,'M');
+        System.out.println(sb);
+
+        //delete
+        // sb.delete(1,1)
+        // System.out.println(sb);
     }
 }
