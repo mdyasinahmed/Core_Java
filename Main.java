57122ba5173f6ea9c00da8f7cff7d1785879a855
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-11 09:10:34 +0600

    Java recap practice - printing code added

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String args[]){
+         // this is java single line comment
+        /**
+         * this is java multiline comment
+         */
+        // printing in java
+        System.out.println("Hello World!");
+    }
+}
