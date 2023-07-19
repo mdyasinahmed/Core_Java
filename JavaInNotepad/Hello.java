099b6b9dee182549700b8a7b1eaf35d7eec792b8
mdyasinahmed <yasinahmed7875@gmail.com>
2023-07-19 23:05:31 +0600

    java input output in note pad

--- JavaInNotepad/Hello.java
+++ JavaInNotepad/Hello.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello World!");
+		System.out.println("Enter 2 Values: ");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt(), b = scan.nextInt();
+		int sum = a + b;
+		System.out.println("Sum of 2 Numbers = " + sum);
+		
 	}
 }
