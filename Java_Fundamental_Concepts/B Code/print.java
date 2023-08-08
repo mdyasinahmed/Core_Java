a9de6c70d5699c07d9f1dfad4a4a9fc7872a1ced
mdyasinahmed <yasinahmed7875@gmail.com>
2023-08-08 11:06:34 +0600

    add practice code

---
+++ Java_Fundamental_Concepts/B Code/print.java
@@ -0,0 +1,34 @@
+// public class print{
+//     public static void main(String[] args){
+//         //System.out.println("Hello World");
+//     }
+// }
+
+// 
+
+public class print{
+    public static void main(String[] args){
+
+    int a=12;
+    int b=32;
+    int sum=a+b;
+    // System.out.println(sum);
+    
+
+    int diff=b-a;
+
+    // 
+    
+    int mul=a*b;
+    // System.out.println(mul);
+
+    int div=a/b;
+    // System.out.println(div);
+
+    int mod=a%b;
+    System.out.println(mod);
+    
+     
+    }
+
+}
