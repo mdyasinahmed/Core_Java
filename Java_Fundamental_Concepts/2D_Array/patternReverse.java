816d1076aa466b88006694ec6bb1f8f55ac5ad82
mdyasinahmed <yasinahmed7875@gmail.com>
2023-11-05 23:22:11 +0600

    update java pattern code

--- Java_Fundamental_Concepts/2D_Array/patternReverse.java
+++ Java_Fundamental_Concepts/2D_Array/patternReverse.java
@@ -1,5 +1,11 @@
 public class patternReverse{
     public static void main(String args[]) {
         System.out.println("Pattern from Decreasing Order: ");
+        for(int i=5; i>=0; i--) {
+            for(int j=i; j>=0; j--) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
     }
 }
